5822622fa7584b3dbb4d9437f28a99d33e7357d6
Nikolay <ffftttyu38@gmail.com>
2023-02-25 19:34:35 +0300

    задача 6

---
+++ задача 6/задача 6.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число а");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2==0)
+{
+    Console.Write ("четное");
+}
+else 
+{
+    Console.Write ("нечетное");
+}
